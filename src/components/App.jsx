fb5cf7f4288a4bc041377fa064a47940fc334e99
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-07 10:29:57 +0530

    Changes
    Changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,7 +31,11 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea />
+      <InputArea
+        handleChange={handleChange}
+        inputText={inputText}
+        addItem={addItem}
+      />
       <div>
         <ul>
           {items.map((todoItem, index) => (
